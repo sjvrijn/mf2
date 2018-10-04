ab5a8138ba12a1d974ee0e40e5a52927f827a144
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2018-10-04 13:30:41 +0200

    renamed folder

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('README') as f:
     long_description = f.read()
 
 setup(
-    name='multi-fidelity-functions',
+    name='multifidelityfunctions',
     version='0.0.0',
     description='A collection of analytical benchmark functions in multiple fidelities',
     long_description=long_description,
     author='Sander van Rijn',
     author_email='s.j.van.rijn@liacs.leidenuniv.nl',
-    packages=['multi-fidelity-functions'],
+    packages=['multifidelityfunctions'],
 )
