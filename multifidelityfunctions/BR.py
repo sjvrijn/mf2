96bb14ee582e7629546a00f5f632a1a7e44a596e
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2018-11-16 15:15:03 +0100

    Fixed branin function to use (more) correct definition now

--- multifidelityfunctions/BR.py
+++ multifidelityfunctions/BR.py
@@ -23,9 +23,8 @@ of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 General Public License for more details.
 """
 
-
 @row_vectorize
-def branin_hf(xx):
+def branin_base(xx):
     """
     BRANIN FUNCTION
 
@@ -34,20 +33,44 @@ def branin_hf(xx):
     """
     x1, x2 = xx
 
-    term1a = x1**2 / 4*math.pi**2
-    term1b = 5*x1 / math.pi
-    term1 = x2 - 5.1*term1a + term1b - 6
+    # t = 1 / (8 * math.pi**2)
+    # s = 10
+    # r = 6
+    # c = 5 / math.pi
+    # b = 5.1 / (4 * math.pi**2)
+    # a = 1
+    #
+    # term1 = a * (x2 - b * x1**2 + c * x1 - r)**2
+    # term2 = s * (1 - t) * math.cos(x1)
+    #
+    # return term1 + term2 + s
 
-    term2 = 10*math.cos(x1) * (1 - 1/(8*math.pi))
+    term1 = x2 - (5.1 * (x1**2 / (4*math.pi**2))) + ((5*x1) / math.pi) - 6
+    term2 = (10 * math.cos(x1)) * (1 - (1/(8*math.pi)))
 
-    return 10 + term1**2 + term2 - 22.5*x2
+    return term1**2 + term2 + 10
+
+
+@row_vectorize
+def branin_hf(xx):
+    """
+    BRANIN FUNCTION, HIGH FIDELITY CODE
+    Calls: branin_base
+    This function, from Dong et al. (2015), is used as the "high-accuracy code"
+    version of the function based on the 'traditional' branin function.
+
+    INPUT:
+    xx = [x1, x2]
+    """
+    x1, x2 = xx
+    return branin_base(xx) - 2.25*x2  # 22.5
 
 
 @row_vectorize
 def branin_lf(xx):
     """
     BRANIN FUNCTION, LOWER FIDELITY CODE
-    Calls: branin_hf
+    Calls: branin_base
     This function, from Dong et al. (2015), is used as the "low-accuracy code"
     version of the function branin_hf.
 
@@ -56,14 +79,16 @@ def branin_lf(xx):
     """
     x1, x2 = xx
 
-    term1 = branin_hf([0.7*x1, 0.7*x2]) - 15.75*x2
-    term2 = 20*(0.9 + x1)**2 - 50
+    term1 = branin_base([0.7*x1, 0.7*x2])
+    term2 = 1.575*x2  # 15.75
+    term3 = 2*(.9+x1**2)
+    term4 = 50
 
-    return term1 + term2
+    return term1 - term2 + term3 - term4
 
 
-l_bound = [-5, 10]
-u_bound = [ 0, 15]
+l_bound = [-5,  0]
+u_bound = [10, 15]
 
 branin = BiFidelityFunction(
     u_bound, l_bound,
