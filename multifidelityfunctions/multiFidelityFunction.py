2a4b783617dc9f1c098e74ef1828a0da4c9ba7d6
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2019-01-14 17:57:01 +0100

    Added generalized MultiFidelityFunction class

--- multifidelityfunctions/multiFidelityFunction.py
+++ multifidelityfunctions/multiFidelityFunction.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from collections import namedtuple
+from numbers import Integral
 import numpy as np
 
 BiFidelityFunction = namedtuple('BiFidelityFunction', ['u_bound', 'l_bound', 'high', 'low'])
@@ -33,3 +34,39 @@ def row_vectorize(func):
         except TypeError:
             return func(X)
     return new_func
+
+
+class MultiFidelityFunction:
+
+
+    def __init(self, u_bound, l_bound, functions, fidelity_names=None):
+        self.u_bound = u_bound
+        self.l_bound = l_bound
+
+        self.functions = functions
+        if fidelity_names:
+            # dict-style name-indexing
+            self.fidelity_names = fidelity_names
+            self.fidelity_dict = dict(zip(fidelity_names, functions))
+            # class-style indexing
+            for name, func in zip(fidelity_names, functions):
+                setattr(self, name, func)
+        else:
+            self.fidelity_dict = None
+            self.fidelity_names = None
+
+
+
+
+    @property
+    def ndim(self):
+        return len(self.u_bound)
+
+
+    def __getitem__(self, item):
+        if isinstance(item, Integral):
+            return self.functions[item]
+        elif isinstance(item, str) and self.fidelity_dict:
+            return self.fidelity_dict[item]
+        else:
+            raise ValueError(f'Invalid index')
