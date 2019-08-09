c1e79f787e769a1b038c5c5d7fffe5557b5b93c5
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2019-08-09 17:28:50 +0200

    Now using numpy in Hartmann.py: removed math; factored out inner loop

--- multifidelityfunctions/Hartmann.py
+++ multifidelityfunctions/Hartmann.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import math
+import numpy as np
 
 from .multiFidelityFunction import MultiFidelityFunction, row_vectorize
 """
@@ -17,50 +17,41 @@ __email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
 
 
 # Some constant values
-A = [
+_A = np.array([
     [10.00,  3.0, 17.00,  3.5,  1.7,  8],
     [ 0.05, 10.0, 17.00,  0.1,  8.0, 14],
     [ 3.00,  3.5,  1.70, 10.0, 17.0,  8],
     [17.00,  8.0,  0.05, 10.0,  0.1, 14],
-]
-P = [
+])
+_P = np.array([
     [1312, 1696, 5569,  124, 8283, 5886],
     [2329, 4135, 8307, 3736, 1004, 9991],
     [2348, 1451, 3522, 2883, 3047, 6650],
-    [4047, 8828, 8732, 5743, 1091,  381]
-]
-four_nine_exp = math.exp(-4/9)
+    [4047, 8828, 8732, 5743, 1091,  381],
+])
+_four_nine_exp = np.exp(-4 / 9)
 
 @row_vectorize
 def hartmann6_hf(xx):
-    alpha = [1, 1.2, 3, 3.2]
+    xx = np.array(xx)
+    alpha = np.array([1, 1.2, 3, 3.2])
 
     outer_sum = 2.58
-    for alpha_i, A_i, P_i in zip(alpha, A, P):
-
-        inner_sum = 0
-        for xx_j, A_ij, P_ij in zip(xx, A_i, P_i):
-
-            inner_sum += A_ij * (xx_j-P_ij)**2
-
-        inner_sum = -inner_sum
-        outer_sum += alpha_i * math.exp(inner_sum)
+    for alpha_i, A_i, P_i in zip(alpha, _A, _P):
+        inner_sum = -np.sum((xx - P_i)**2 * A_i)
+        outer_sum += alpha_i * np.exp(inner_sum)
 
     return -(1/1.94) * outer_sum
 
+
 @row_vectorize
 def hartmann6_lf(xx):
-    alpha = [0.5, 0.5, 2, 4]
+    alpha = np.array([0.5, 0.5, 2, 4])
 
     outer_sum = 2.58
-    for alpha_i, A_i, P_i in zip(alpha, A, P):
+    for alpha_i, A_i, P_i in zip(alpha, _A, _P):
 
-        inner_sum = 0
-        for xx_j, A_ij, P_ij in zip(xx, A_i, P_i):
-
-            inner_sum += A_ij * (xx_j-P_ij)**2
-
-        inner_sum = -inner_sum
+        inner_sum = -np.sum((xx - P_i)**2 * A_i)
         outer_sum += alpha_i * f_exp(inner_sum)
 
     return -(1/1.94) * outer_sum
@@ -68,7 +59,7 @@ def hartmann6_lf(xx):
 
 def f_exp(x):
 
-    return (four_nine_exp + four_nine_exp*(math.exp((x+4)/9)))**9
+    return (_four_nine_exp + _four_nine_exp * (np.exp((x + 4) / 9))) ** 9
 
 
 l_bound = [0.1] * 6
