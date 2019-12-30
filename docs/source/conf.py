dcf1b5973977e7488164bf8bdd73c265d694bbe0
Sander van Rijn <svr003@gmail.com>
2019-12-30 15:37:45 +0100

    Updated project display name in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Multi-Fidelity Functions'
+project = 'MF2: Multi-Fidelity Functions'
 copyright = '2019, Sander van Rijn'
 author = 'Sander van Rijn'
 
