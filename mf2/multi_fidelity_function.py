bab3d13b7e29697bd9e4773f706ca7129d2915e0
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2022-06-02 14:39:47 +0200

    Add AdjustableMultiFidelityFunction class

--- mf2/multi_fidelity_function.py
+++ mf2/multi_fidelity_function.py
@@ -8,6 +8,7 @@ parameters of a multi-fidelity function. Also contains any other utility
 functions that are commonly used by the various mf-functions in this package.
 """
 
+from functools import partial
 from numbers import Integral
 from typing import Callable
 from warnings import warn
@@ -38,7 +39,6 @@ class MultiFidelityFunction:
                                (if known).
         """
         self._name = name
-
         self.u_bound = np.array(u_bound, dtype=float)
         self.l_bound = np.array(l_bound, dtype=float)
         self._check_bounds()
@@ -115,6 +115,73 @@ class MultiFidelityFunction:
 
     def __repr__(self):
         return f"MultiFidelityFunction({self.name}, {self.u_bound}, {self.l_bound}, fidelity_names={self.fidelity_names})"
+
+
+class AdjustableMultiFidelityFunction:
+
+    def __init__(self, name, u_bound, l_bound, static_functions,
+                 adjustable_functions, fidelity_names=None,
+                 *, x_opt=None):
+        """All fidelity levels and parameters of a multi-fidelity function.
+
+        :param name:                  Name of the multi-fidelity function.
+        :param u_bound:               Upper bound of the intended input range.
+                                      Length is also used to determine the
+                                      (fixed) dimensionality of the function.
+        :param l_bound:               Lower bound of the intended input range.
+                                      Must be of same length as `u_bound`.
+        :param static_functions:      Iterable of function handles for the
+                                      static, non-adjustable fidelities,
+                                      sorted in *descending* order.
+        :param adjustable_functions:  Iterable of function handles for the
+                                      adjustable fidelities, sorted in
+                                      *descending* order.
+        :param fidelity_names:        List of names for the fidelities. Must be
+                                      given to support dictionary- or attribute-
+                                      style fidelity indexing, such as
+                                      `f['high']()` and `f.high()`
+        :param x_opt:                 Location of optimum x_opt for highest
+                                      fidelity (if known).
+        """
+        self._name = name if name.startswith('adjustable') else f'adjustable {name}'
+        self.u_bound = np.array(u_bound, dtype=float)
+        self.l_bound = np.array(l_bound, dtype=float)
+        self.static_functions = static_functions
+        self.adjustable_functions = adjustable_functions
+        self.fidelity_names = fidelity_names
+        self.x_opt = x_opt
+
+
+    def __call__(self, a: float) -> MultiFidelityFunction:
+        """Fix adjustment to create a MultiFidelityFunction"""
+        return MultiFidelityFunction(
+            f'{self._name} {a}',
+            self.u_bound, self.l_bound,
+            self.static_functions + [partial(f, a=a) for f in self.adjustable_functions],
+            fidelity_names=self.fidelity_names,
+        )
+
+
+    @property
+    def name(self):
+        return self._name.title()
+
+
+    @property
+    def ndim(self):
+        """Dimensionality of the function. Inferred as ``len(self.u_bound)``."""
+        return len(self.u_bound)
+
+
+    @property
+    def functions(self):
+        """Combined static and adjustable functions"""
+        return self.static_functions + self.adjustable_functions
+
+
+    def __repr__(self):
+        return f"AdjustableMultiFidelityFunction({self.name}, {self.u_bound}," \
+               f"{self.l_bound}, fidelity_names={self.fidelity_names})"
 
 
 def invert(mff: MultiFidelityFunction) -> MultiFidelityFunction:
